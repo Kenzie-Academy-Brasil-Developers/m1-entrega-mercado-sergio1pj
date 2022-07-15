1a4520dcd39d4f4cfd7a729b3f1cad2e19464a5d
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-07-15 01:41:31 -0300

    bugs corrigidos, era só innerText que faltava :(

--- index.js
+++ index.js
@@ -1,8 +1,103 @@
+const products = [
+    {
+      title: "Uva Crimson",
+      price: 8.99,
+      category: "Frutas",
+      image: undefined,
+      imageDescription: "",
+    },
+    {
+      title: "Banana",
+      price: 5.69,
+      category: "Frutas",
+      image: "./img/products/product_2.svg",
+      imageDescription: "",
+    },
+    {
+      title: "Mamão",
+      price: 4.99,
+      category: "Frutas",
+      image: "./img/products/product_3.svg",
+      imageDescription: "",
+    },
+    {
+      title: "Maçã",
+      price: 9.2,
+      category: "Frutas",
+      image: "./img/products/product_4.svg",
+      imageDescription: "",
+    },
+    {
+      title: "Refrigerante",
+      price: 8.99,
+      category: "Bebidas",
+      image: undefined,
+      imageDescription: "",
+    },
+    {
+      title: "Vinho",
+      price: 8.99,
+      category: "Bebidas",
+      image: "./img/products/product_6.svg",
+      imageDescription: "",
+    },
+    {
+      title: "Água Tônica",
+      price: 8.99,
+      category: "Bebidas",
+      image: undefined,
+      imageDescription: "",
+    },
+    {
+      title: "Água de coco",
+      price: 8.99,
+      category: "Bebidas",
+      image: "./img/products/product_8.svg",
+      imageDescription: "",
+    },
+  
+    {
+      title: "Sabonete",
+      price: 8.99,
+      category: "Higiene",
+      image: "./img/products/product_9.svg",
+      imageDescription: "",
+    },
+  
+    {
+      title: "Detergente",
+      price: 8.99,
+      category: "Higiene",
+      image: "./img/products/product_10.svg",
+      imageDescription: "",
+    },
+  
+    {
+      title: "Limpa superfícies",
+      price: 8.99,
+      category: "Higiene",
+      image: "./img/products/product_11.svg",
+      imageDescription: "",
+    },
+  
+    {
+      title: "Lustra Móveis",
+      price: 8.99,
+      category: "Higiene",
+      image: undefined,
+      imageDescription: "",
+    },
+  ];
+const translatedCategory = {
+    Bebidas : "drinks",
+    Frutas : "fruits",
+    Higiene : "hygiene"
+}
 let fruits = document.querySelector(".fruits");
 let drinks = document.querySelector(".drinks");
 let hygiene = document.querySelector(".hygiene");
 function appendChildren(childrenElements, parentElement) {
-    childrenElements.forEach(child => parentElement.appendChild(child));
+    childrenElements.forEach(childElement => parentElement.appendChild(childElement));
     return "parentElement foi atualizado com sucesso";
 }
 function createProductCard(product) {
@@ -12,9 +107,9 @@ function createProductCard(product) {
     let productTitle = document.createElement("h1");
     let productCategory =  document.createElement("h5");
     let productPrice =  document.createElement("strong");
-    productImg.src = product.image;
+    productImg.src = product.image !== undefined ? product.image : "./img/products/no-img.svg";
     productImg.alt = product.imageDescription;
-    productTitle = product.title;
+    productTitle.innerText = product.title;
     productCategory.innerText = product.category;
     productPrice.innerText = product.price;
     productCard.classList.add("product");
@@ -25,5 +120,18 @@ function createProductCard(product) {
     productPrice.classList.add("product-price");
     appendChildren([productTitle, productCategory, productPrice], productMain);
     appendChildren([productImg, productMain], productCard);
-    return "ProductCard foi criado com sucesso";
+    return productCard;
 }
+function modSection(element, list) {
+    let ul = document.createElement("ul");
+    let elementClassName = element.className
+    let subList = list.filter(product => elementClassName.includes(translatedCategory[product.category]));
+    let subListElements = subList.map(product => createProductCard(product))
+    appendChildren(subListElements, ul);
+    console.log(ul)
+    element.appendChild(ul);
+    return "Modificado com sucesso";
+}
+modSection(fruits, products);
+modSection(drinks, products);
+modSection(hygiene, products);
